a1df8cd8bc2a14caee4277a72d089401a5e8e740
davidtavartk <d.tavartkiladze5@gmail.com>
2025-06-02 22:35:29 +0400

    feat: add track logic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
---
+++ src/track/track.service.ts
@@ -0,0 +1,51 @@
+// track/track.service.ts
+import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
+import { Track } from './entities/track.entity';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { UpdateTrackDto } from './dto/update-track.dto';
+
+@Injectable()
+export class TrackService {
+  private tracks: Track[] = [];
+
+  async getAllTracks() {
+    return this.tracks;
+  }
+
+  async getTrackById(id: string) {
+    return this.tracks.find((track) => track.id === id) || null;
+  }
+
+  async create(createTrackDto: CreateTrackDto) {
+    const newTrack = new Track();
+    newTrack.id = randomUUID();
+    newTrack.name = createTrackDto.name;
+    newTrack.artistId = createTrackDto.artistId;
+    newTrack.albumId = createTrackDto.albumId;
+    newTrack.duration = createTrackDto.duration;
+
+    this.tracks.push(newTrack);
+    return newTrack;
+  }
+
+  async update(id: string, updateTrackDto: UpdateTrackDto) {
+    const track = this.tracks.find((t) => t.id === id);
+    if (!track) return null;
+
+    track.name = updateTrackDto.name;
+    track.artistId = updateTrackDto.artistId;
+    track.albumId = updateTrackDto.albumId;
+    track.duration = updateTrackDto.duration;
+
+    return track;
+  }
+
+  async remove(id: string) {
+    const trackIndex = this.tracks.findIndex((t) => t.id === id);
+    if (trackIndex === -1) return false;
+
+    this.tracks.splice(trackIndex, 1);
+    return true;
+  }
+}
