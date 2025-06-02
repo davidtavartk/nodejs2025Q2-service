48c254a3fd7935cb29149e2b0d0fe89a940df17c
davidtavartk <d.tavartkiladze5@gmail.com>
2025-06-02 23:14:09 +0400

    feat: finalize endpoints

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { FavoritesController } from './favorites.controller';
+import { FavoritesService } from './favorites.service';
+import { ArtistModule } from '../artist/artist.module';
+import { AlbumModule } from '../album/album.module';
+import { TrackModule } from '../track/track.module';
 
-@Module({})
+@Module({
+  imports: [ArtistModule, AlbumModule, TrackModule],
+  controllers: [FavoritesController],
+  providers: [FavoritesService],
+  exports: [FavoritesService],
+})
 export class FavoritesModule {}
